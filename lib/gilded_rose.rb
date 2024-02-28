3c44589bfc289bd58823512cdb3ba89523320006
CawWoo <melodicraven@gmail.com>
2024-02-27 20:47:22 -0600

    Changed small method style to small object style

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,50 +1,54 @@
-class GildedRose
-  attr_reader :name, :days_remaining, :quality
+module GildedRose
+  class Item
+    attr_reader :quality, :days_remaining
 
-  def initialize(name:, days_remaining:, quality:)
-    @name, @days_remaining, @quality = name, days_remaining, quality
-  end
+    def initialize(quality, days_remaining)
+      @quality, @days_remaining = quality, days_remaining
+    end
 
-  def tick
-    case name
-    when 'Normal Item'
-      normal_tick
-    when 'Aged Brie'
-      brie_tick
-    when 'Sulfuras, Hand of Ragnaros'
-      sulfuras_tick
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      backstage_tick
+    def tick
     end
   end
 
+  class Normal < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
 
-  def normal_tick
-    @days_remaining -= 1
-    return if @quality == 0
-
-    @quality -= 1
-    @quality -= 1 if @days_remaining <= 0
+      @quality -= 1
+      @quality -= 1 if days_remaining <= 0
+    end
   end
 
-  def brie_tick
-    @days_remaining -= 1
-    return if @quality >= 50
+  class Brie < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
 
-    @quality += 1
-    @quality += 1 if @days_remaining <= 0 && @quality < 50
+      @quality += 1
+      @quality += 1 if @days_remaining <= 0 && @quality < 50
+    end
   end
 
-  def sulfuras_tick
+  class Backstage < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality >= 50
+      return @quality = 0 if @days_remaining < 0
+
+      @quality += 1
+      @quality += 1 if days_remaining < 10
+      @quality += 1 if days_remaining < 5
+    end
   end
 
-  def backstage_tick
-    @days_remaining -= 1
-    return if @quality >= 50
-    return @quality = 0 if @days_remaining < 0
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'Normal Item' => Normal,
+    'Aged Brie' => Brie,
+    'Backstage passes to a TAFKAL80ETC concert' => Backstage }
 
-    @quality += 1
-    @quality += 1 if days_remaining < 10
-    @quality += 1 if days_remaining < 5
+  def self.new(name:, quality:, days_remaining:)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
